3282d8cff0622c914ac57439befb2d573f2762cb
Lachlan P <lwpearce@gmail.com>
2025-08-02 17:19:38 +1000

    Update test config

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js'],
+  testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.js", "**/tests/**/*.test.ts"],
   collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html', 'json-summary'],
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '/tests/',
-    '/coverage/'
-  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html", "json-summary"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/tests/", "/coverage/"],
   testTimeout: 60000, // 60 seconds timeout for video generation tests
   verbose: true,
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
-}; 
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "js", "json"],
+  preset: "ts-jest/presets/default",
+};
